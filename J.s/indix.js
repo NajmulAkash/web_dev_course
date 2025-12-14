65f74f08d42e483faf2c847f5da78d2aa2d0dcfb
Najmul Akash <najmulbalghari23@gmail.com>
2025-12-14 02:53:54 -0800

    js

---
+++ J.s/indix.js
@@ -0,0 +1,20 @@
+let a=1;
+a=a+2;
+if(a>80==a<=100){
+    console.log("A+");
+}
+else if(a>50==a<=70){
+    console.log("A");
+}
+else if(a>30==a<=50){
+    console.log("B");
+}
+else if(a>10==a<=30){
+    console.log("C");
+}
+else if(a>1==a<=10){
+    console.log("Fail");
+}
+else{
+    console.log("Top");
+}
