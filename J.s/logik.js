b6abf94dee8ca03e0bd5ed64027ac472dc0b7706
Najmul Akash <najmulbalghari23@gmail.com>
2025-12-16 02:13:30 -0800

    home work

--- J.s/logik.js
+++ J.s/logik.js
@@ -1,32 +1,32 @@
-// const pre="+";
-// switch (pre) {
-//     case "+":
-//         console.log("pre",9+5);5
-//         break;
-//         case "-":
-//             console.log("pre",5+6);
-//         break;
-// }
-// let a=5
-// let b=2
-// let c=4
-// for(let i=1;i<=10;i++){
-//     console.log(i);
-//     if(i==3 || i==6){
-//         continue
-//     }
-//     else{
-//         console.log(i)
-//     }
-// }
-//  let d=5
-// while(d<=10){
-//     console.log(d);
-//    d++;
-// }
-// console.log("do")
-// do{
-//     console.log(d)
-//     d++
-// }
-// while(d<10)
+const pre="+";
+switch (pre) {
+    case "+":
+        console.log("pre",9+5);5
+        break;
+        case "-":
+            console.log("pre",5+6);
+        break;
+}
+let a=5
+let b=2
+let c=4
+for(let i=1;i<=10;i++){
+    console.log(i);
+    if(i==3 || i==6){
+        continue
+    }
+    else{
+        console.log(i)
+    }
+}
+ let d=5
+while(d<=10){
+    console.log(d);
+   d++;
+}
+console.log("do")
+do{
+    console.log(d)
+    d++
+}
+while(d<10)
--- J.s/task.js
+++ J.s/task.js
@@ -1,7 +1,28 @@
-// let a=4;
-// for(a>=0;a--;){
+
+// let arr=[1,3,4,6,8,9]
+// console.log(arr);
+// console.log(arr[0]);
+
+
+// divisibal by 2&5
+// for(let a=1;a<=50;a++){
+//     if(a%3==0 && a%5==0)
 //     console.log(a);
 // }
-for(let a=1;a<50;a++){
-    console.log(a);
-}
+
+
+// reverse loop ;
+// let a=4;
+// for(a>0;a--;){
+//     console.log(a)
+// }
+
+
+// prime number
+// for(let b=1;b<50;b++){
+//     if(b%2==0)
+//     console.log(b)
+// }
+
+
+
