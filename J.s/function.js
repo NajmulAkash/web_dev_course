ce23d79d6930fff2cf45cb47c503b6b21473fe8a
Najmul Akash <najmulbalghari23@gmail.com>
2025-12-23 02:21:27 -0800

    kam

---
+++ J.s/function.js
@@ -0,0 +1,73 @@
+// function message print
+
+// function MyFunction(msg) {
+//     console.log(msg);
+// }
+// MyFunction ("I love Js");
+
+
+
+// return sum 2number
+
+// function sum(a,b){
+//     s = a + b;
+//     return s;
+// };
+// let val = sum(4,4);
+// console.log(val);
+//  let ali =(a,b)=>{
+//     console.log(a*b);
+// }
+// ali (2,5);
+
+
+// count vowel
+
+// function countvowels(str) {
+//     let countvol = 0;
+//     let countcon = 0;
+//     for(const char of str){
+//        if(char === "a" ||  char === "e" ||  char === "i" || char === "o" || char === "u") {
+//         countvol++;
+//        }
+//        else if(
+//           char === "a" || 
+//           char === "e" || 
+//           char === "i" || 
+//           char === "o" || 
+//           char === "u"
+//         ){
+//             countcon ++;
+//         }
+//     }
+//     console.log(countvol);
+//     console.log(countcon)
+// }
+// countvowels("Najmul akash")
+
+
+
+// arrow function count vowel
+
+// const countvow = (str)=>{
+//     count = 0;
+// for(const char of str){
+//     if(char==="a"|| char==="e"|| char==="i"|| char==="o"|| char==="u"){
+//         count++;
+//     }
+// }
+// console.log(count);
+// }
+// countvow("najmul akash");
+
+
+// push Array;
+// let arr=["ali" ,"inam" ,"Najmul"];
+// arr.push("Yaseen");
+// console.log(arr);
+
+// functionm plus more number
+function sum(a,...b){
+    return a+b;
+}
+console.log(sum(2,4,4,3,3,7));
