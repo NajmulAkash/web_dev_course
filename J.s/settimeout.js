eb770ce4e50eb4d010ffb8e5b8a5cd7b2e11fc20
Najmul Akash <najmulbalghari23@gmail.com>
2025-12-24 03:22:47 -0800

    responsive website

---
+++ J.s/settimeout.js
@@ -0,0 +1,32 @@
+// let najmul=setTimeout(() => {
+//     console.log("run");
+// },1000);
+// let akash=setTimeout(() => {
+//     console.log("running");
+// },);
+
+let person={
+    name:"Najmul Akash",
+    FatherName:"Ejaz Hussain",
+    RollNo:"966",
+    Class:"9th",
+    mark :{
+        physics:"89%",
+        Hardware:"100%",
+        Software :"60%",
+        result :{
+            position:"3th",
+        }
+    }
+}
+    setTimeout(() => {
+        console.log("Physics Mark:",person.mark.physics);
+    }, 4000);
+
+console.log("Name:",person.name)
+console.log("Fahter Name:",person.FatherName)
+console.log("Roll Number:",person.RollNo)
+console.log("Class:",person.Class)
+console.log("Physics Mark:",person.mark.physics)
+console.log("Hardware Mark:",person.mark.Hardware)
+console.log("Software:",person.mark.Software)
