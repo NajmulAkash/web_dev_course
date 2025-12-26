dc4b1edad1bbe633e4e0db7b5a9718266447e952
Najmul Akash <najmulbalghari23@gmail.com>
2025-12-25 23:21:21 -0800

    ali

--- J.s/settimeout.js
+++ J.s/settimeout.js
@@ -5,28 +5,59 @@
 //     console.log("running");
 // },);
 
+// let person={
+//     name:"Najmul Akash",
+//     FatherName:"Ejaz Hussain",
+//     RollNo:"966",
+//     Class:"9th",
+//     mark :{
+//         physics:"89%",
+//         Hardware:"100%",
+//         Software :"60%",
+//         result :{
+//             position:"3th",
+//         }
+//     }
+// }
+//     setTimeout(() => {
+//         console.log("Physics Mark:",person.mark.physics);
+//     }, 2000);
+
+// console.log("Name:",person.name)
+// console.log("Fahter Name:",person.FatherName)
+// console.log("Roll Number:",person.RollNo)
+// console.log("Class:",person.Class)
+// console.log("Hardware Mark:",person.mark.Hardware)
+// console.log("Software:",person.mark.Software)
+
+
+setTimeout(() => {
+    console.log("program 1")
+},5000);
+setTimeout(() => {
+    console.log("program 2")
+},2000);
+setTimeout(() => {
+    console.log("program 3")
+},4000);
+setTimeout(() => {
+    console.log("program 4")
+},3000);
+setTimeout(() => {
+    console.log("program 5")
+},1000);
+
 let person={
-    name:"Najmul Akash",
-    FatherName:"Ejaz Hussain",
-    RollNo:"966",
-    Class:"9th",
-    mark :{
-        physics:"89%",
-        Hardware:"100%",
-        Software :"60%",
-        result :{
-            position:"3th",
+    name:"ali",
+    roll:"374",
+    class:"9th",
+    Address:{
+        PermenentAddress:"Balghar",
+        LocalAddress:"Skardu",
+        galeNo:"88439",
+        mark:{
+            java:"83",
+            python:"3738",
         }
     }
 }
-    setTimeout(() => {
-        console.log("Physics Mark:",person.mark.physics);
-    }, 4000);
-
-console.log("Name:",person.name)
-console.log("Fahter Name:",person.FatherName)
-console.log("Roll Number:",person.RollNo)
-console.log("Class:",person.Class)
-console.log("Physics Mark:",person.mark.physics)
-console.log("Hardware Mark:",person.mark.Hardware)
-console.log("Software:",person.mark.Software)
