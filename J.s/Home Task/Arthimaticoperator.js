5cb0545c742cb5f14af8d95d6e1aa95b4284d46e
Najmul Akash <najmulbalghari23@gmail.com>
2026-01-27 02:15:05 -0800

    ali

---
+++ J.s/Home Task/Arthimaticoperator.js
@@ -0,0 +1,47 @@
+//Addition //
+let a = 5;
+let b = 2;
+let c = a+b;
+console.log(c);
+
+//Subtracion//
+{
+    let a=4
+    let b=2
+    let c= a-b;
+    console.log(c)
+}
+
+//Multiplaction//
+{
+    let a=2;
+    let b=4;
+    let c= a*b;
+    console.log(c)
+}
+
+//if condition//
+{
+    let a=3;
+    
+    if(a%2==0){
+        console.log("Even Number")
+    }
+    else {
+        console.log("Old Number")
+    }
+}
+
+//    //
+{
+    let a=6;
+    
+    if(a>=0 == a>=90){
+        console.log("Even Number")
+    }
+    else {
+        console.log("Old Number")
+    }
+}
+
+
