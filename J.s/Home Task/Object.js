34a11e1e5d44ba6aa568287d255b7ba00b3c8257
Najmul Akash <najmulbalghari23@gmail.com>
2026-01-29 08:53:37 -0800

    object

---
+++ J.s/Home Task/Object.js
@@ -0,0 +1,15 @@
+let akash={
+    name:"Najmul Akash",
+    fname:"Ejaz Hussain",
+    class:"9th",
+    school:"Mid-City School and College",
+    Accadmy:"Next-planer Accadmy",
+    Skill:{
+        java:"20%",
+        html:"30%",
+        css:"90%",
+        boostrep:"40%",
+        python:"30%",
+    }
+}
+console.log(akash);
