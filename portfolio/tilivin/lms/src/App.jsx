d2e2da065ad6288fbb631ecfe5d3d8fc1c6e752e
Najmul Akash <najmulbalghari23@gmail.com>
2026-01-30 04:54:40 -0800

    Portfolio

--- portfolio/tilivin/lms/src/App.jsx
+++ portfolio/tilivin/lms/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Portfolio from './Home task/Portfolio'
+import Flex from './Npa/Flex'
 
 function App() {
   return (
     <>
     <Portfolio/>
+    <Flex/>
     </>
   )
 }
