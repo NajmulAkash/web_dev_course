a42aaaee8c99481f243e4c9f6fae7e9c8eb04275
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-12 04:13:07 -0800

    ali

--- portfolio/tilivin/lms/src/App.jsx
+++ portfolio/tilivin/lms/src/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
-import Heros from './Home task/Heros'
-import Navbar from './Home task/Navbar'
-import TourisAbout from './Home task/TourisAbout'
-import TravelFooters from './Home task/TravelFooters'
+import Facbook from './Facebook'
 function App() {
   return (
     <>
-    <Navbar/>
-   <Heros/>
-   <TourisAbout/>
-   <TravelFooters/>
+ <Facbook/>
     </>
   )
 }
