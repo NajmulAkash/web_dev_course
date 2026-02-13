27b13376815c556101fb8c0da8e4a090302f1456
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-13 07:34:46 -0800

    object

---
+++ J.s/Home Task/Obje.js
@@ -0,0 +1,18 @@
+let inam={
+    name:"Inam Ullah",
+    FName:"Ejaz Hussain",
+    Class:"9th",
+    Skills:{
+        Html:"85%",
+        Css:"39%",
+        Boostrep:"84%",
+        Js:"38%",
+        react:"30%",
+        TailwindCss:"95%",
+        Exprences:{
+            exprences:"Four Year Exprences",
+            Pro:"Ultra Pro Ha html css boostrep tailwind css react js SQl"
+        }
+    }
+
+}
