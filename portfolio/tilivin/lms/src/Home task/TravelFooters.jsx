30fe4ab053b461177832ad1dd2d23ce952663b7c
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-16 04:30:06 -0800

    navbar

--- portfolio/tilivin/lms/src/Home task/TravelFooters.jsx
+++ portfolio/tilivin/lms/src/Home task/TravelFooters.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
-
+import HeroS from '../Npa/HeroS'
+import Portfolios from '../Npa/Portfolios'
 function TravelFooters() {
   return (
     <>
-    <div className='w-full h-fit bg-gray-800 '>
-      <div className='max-w-7xl px-8 py-4 bg-red-500 mx-28 flex'>
-        <h1 className='text-white text-3xl tracking-wide'>Visit Gilgit</h1>
-        <div className='space-x-7'>
-          <a href="">Home</a>
-        </div>
-      </div>
-        </div>
+    <Portfolios/>
+    <HeroS/>
     </>
   )
 }
