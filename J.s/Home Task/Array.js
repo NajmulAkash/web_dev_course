c45bcd1269a045392a0cfa658dfcac1354974640
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-19 03:01:36 -0800

    array

---
+++ J.s/Home Task/Array.js
@@ -0,0 +1,17 @@
+let arr =[1,2,3,4,5,6,7]
+console.log(arr)
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+arr.push(9)
+arr.unshift(3)
+console.log(arr)
+arr.unshift(-1)
+console.log(arr)
+
+let a=[1,2,3,4,5,6]
+console.log(a)
+a.pop()
+a.pop()
+console.log(a)
