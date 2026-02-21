78e770b90f64ce2369960f96799e582def05a223
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-21 02:21:33 -0800

    n

---
+++ J.s/Home Task/Settimeout.js
@@ -0,0 +1,67 @@
+setTimeout(() => {
+   console.log("Program") 
+}, 3000);
+setTimeout(() => {
+    console.log("Program 3")
+}, 2000);
+setTimeout(() => {
+    console.log("Program 2")
+}, 1000);
+setTimeout(() => {
+   console.log("Program") 
+}, 3000);
+setTimeout(() => {
+    console.log("Program 3")
+}, 2000);
+setTimeout(() => {
+    console.log("Program 2")
+}, 1000);
+setTimeout(() => {
+   console.log("Program") 
+}, 3000);
+setTimeout(() => {
+    console.log("Program 3")
+}, 2000);
+setTimeout(() => {
+    console.log("Program 2")
+}, 1000);setTimeout(() => {
+   console.log("Program") 
+}, 3000);
+setTimeout(() => {
+    console.log("Program 3")
+}, 2000);
+setTimeout(() => {
+    console.log("Program 2")
+}, 1000);setTimeout(() => {
+   console.log("Program") 
+}, 3000);
+setTimeout(() => {
+    console.log("Program 3")
+}, 2000);
+setTimeout(() => {
+    console.log("Program 2")
+}, 1000);setTimeout(() => {
+   console.log("Program") 
+}, 3000);
+setTimeout(() => {
+    console.log("Program 3")
+}, 2000);
+setTimeout(() => {
+    console.log("Program 2")
+}, 1000);setTimeout(() => {
+   console.log("Program") 
+}, 3000);
+setTimeout(() => {
+    console.log("Program 3")
+}, 2000);
+setTimeout(() => {
+    console.log("Program 2")
+}, 1000);setTimeout(() => {
+   console.log("Program") 
+}, 3000);
+setTimeout(() => {
+    console.log("Program 3")
+}, 2000);
+setTimeout(() => {
+    console.log("Program 2")
+}, 1000);
