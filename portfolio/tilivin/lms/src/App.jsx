745753fdbf2a27ba19c7f5ffb68d9039a53be57f
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-24 02:52:13 -0800

    n

--- portfolio/tilivin/lms/src/App.jsx
+++ portfolio/tilivin/lms/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Google from './PanoteTask/Google'
 import AddShortCut from './PanoteTask/AddShortCut'
+import { Route, Routes } from 'react-router-dom'
+import About from './PanoteTask/About'
+
 function App() {
   return (
     <>
-    <Google/>
-   {/* <AddShortCut/> */}
+      <Routes>
+        <Route path='/' element={<Google />} />
+        <Route path='/AddShortCut' element={<AddShortCut />} />
+        <Route path='/About' element={<About />} />
+      </Routes>
     </>
   )
 }
--- portfolio/tilivin/lms/src/PanoteTask/About.jsx
+++ portfolio/tilivin/lms/src/PanoteTask/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from '../Image/nnnn.png'
+import { Link } from 'react-router-dom'
 function About() {
   return (
    <>
