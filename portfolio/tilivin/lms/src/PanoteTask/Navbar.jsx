2c2e653062385a0409bee0c75db86fcae9ce6d8c
Najmul Akash <najmulbalghari23@gmail.com>
2026-02-26 08:52:56 -0800

    sidebar

--- portfolio/tilivin/lms/src/PanoteTask/Navbar.jsx
+++ portfolio/tilivin/lms/src/PanoteTask/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Navbar() {
+    const [Item,setItem]=useState(false)
   return (
   <>
   <nav className='bg-pink-600'>
@@ -12,13 +13,15 @@ function Navbar() {
             <a href="" className='px-4 text-2xl hover:text-white'>Skills</a>
             <a href="" className='px-4 text-2xl hover:text-white'>Contact</a>
         </div>
-          <div className='block  sm:hidden'>
-            <a href="" className='px-4 text-2xl hover:text-white'>Home</a>
-            <a href="" className='px-4 text-2xl hover:text-white'>About</a>
-            <a href="" className='px-4 text-2xl hover:text-white'>Skills</a>
-            <a href="" className='px-4 text-2xl hover:text-white'>Contact</a>
-        </div>
+        <button onClick={()=>setItem(!Item)}>Open</button>
+       
     </div>
+       <div className={`${Item ? "block" :"hidden"}  sm:hidden bg-red-500 space-y-2`}>
+            <a href="" className='px-4 text-2xl hover:text-white block '>Home</a>
+            <a href="" className='px-4 text-2xl hover:text-white block '>About</a>
+            <a href="" className='px-4 text-2xl hover:text-white block '>Skills</a>
+            <a href="" className='px-4 text-2xl hover:text-white block '>Contact</a>
+        </div>
   </nav>
   </>
   )
